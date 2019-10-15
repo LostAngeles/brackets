0fc22b6097a4c227e867cb17da3b5987626284c6
LostAngeles <55195328+LostAngeles@users.noreply.github.com>
2019-10-16 00:14:45 +0300

    feat: brackets  solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) {  
+
+  if (str.length % 2 != 0) return false;
+
+  let checker = 1;
+	let tempString = "";
+	while ((str.length > 0) && checker > 0) {
+		checker = bracketsConfig.length;
+		for (let i = 0; i < bracketsConfig.length; i++) {
+			tempString = bracketsConfig[i].join();
+			tempString = tempString.replace(",", "");			
+			if (str.includes(tempString)) {
+				str = str.replace(tempString, "");
+			}
+			else {checker = checker - 1};			
+		}
+	}
+
+	if (str.length == 0) return true;
+	else if (str.length > 0) return false;
 }
